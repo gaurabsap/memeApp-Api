5f8365402e377da359f11710f9a36c914a52c642
gb_gaurab19 <86666317+gaurabsap@users.noreply.github.com>
2023-06-29 09:14:57 +0545

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json({limit: '200mb'}));
 app.use(express.urlencoded({limit: '200mb', extended: false}));
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: true
 }))
 app.use('/api/users', routes)
 app.use('/api', memes)
